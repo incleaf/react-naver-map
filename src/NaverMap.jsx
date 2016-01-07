6ecd7c0248668aff28fd25d09b1582a995a63d0f
Hyeon-Su Lee <incleaf@gmail.com>
2016-01-08 02:14:58 +0900

    Configure project structure

---
+++ src/NaverMap.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+
+const NaverMap = React.createClass({
+    render () {
+        const text = "Hello Naver Map";
+        return (
+            <div>
+                {this.text}
+            </div>
+        )
+    }
+})
+
+export default NaverMap
